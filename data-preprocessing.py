a543846c78873e959f200e8066a6d737466295ee
srh95 <srh95@case.edu>
2023-04-04 15:28:26 -0400

    add speaker and subject attributes to csv files

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -1,24 +1,25 @@
 import pandas as pd
 
-file_name = "Liar-dataset/test.tsv"
+file_name = "Liar-dataset/valid.tsv"
 
 data = pd.read_csv(file_name, sep='\t', index_col=0)
 
-# Remove irrelevant columns (keep only statement and label)
-cols = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# Remove irrelevant columns (keep only statement, label, speaker, and subject)
+print(data)
+cols = [4, 5, 6, 7, 8, 9, 10, 11, 12]
 data.drop(data.columns[cols], axis=1, inplace=True)
-
+print(data)
 
 # Delete any rows with null or blank value
 data.dropna(axis=0, how='any', inplace=True)
 
 # Name columns
-data.columns =['Label', 'Statement']
+data.columns =['Label', 'Statement', 'Subject', 'Speaker']
 
 # Convert labels to true and false 
 data['Label'] = data['Label'].replace(['true', 'mostly-true', 'half-true'], 'TRUE')
 data['Label'] = data['Label'].replace(['barely-true', 'false', 'pants-fire'], 'FALSE')
 
 # Output as csv file
-header = ['Statement', 'Label']
-data.to_csv("test.csv", columns = header, index=False)
+header = ['Statement', 'Label', 'Subject', 'Speaker']
+data.to_csv("four-variable-files/valid.csv", columns = header, index=False)
